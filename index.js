7496563abe4cbeb7eadae7ad127642299aa6c193
estein1988 <estein1988@gmail.com>
2020-08-31 10:48:16 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, day) {
+    let thankYouCards = []
+    for (let i = 0; i < names.length; i++) {
+        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${day} gift!`);
+    }
+    return thankYouCards;
+}
+
+
+function countDown(number) {
+    while (number > 0) {
+        console.log(number);
+        number -= 1;
+    }
+    console.log(number);
+}
+

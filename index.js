75a84f76dc92cd1ec90f63332ed1bc77f640b5b7
bgordon8 <gordonbrian16@gmail.com>
2020-09-08 18:22:02 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const stringNames = ["Lisa", "Kaitlin", "Jan"]
+const eventName = "surprise"
+let newMessageArray = []
+
+function writeCards(stringNames, eventName) {
+    for (let i = 0; i < stringNames.length; i++) {
+        newMessageArray.push(`Thank you, ${stringNames[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return newMessageArray
+}
+
+
+function countDown() {
+    let count = 10;
+    while (count >= 0) {
+        console.log(count--)
+    }
+}
+
